d22b2e65e42358bf7950226c10777c783da1e887
Gabriele Greco <gabrielegreco395@gmail.com>
2024-07-03 19:33:11 +0100

    updated region section and graphics

--- script.js
+++ script.js
@@ -54,7 +54,84 @@ fetch('https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-json/dpc-c
 
     })();
 
+    let regions = document.querySelectorAll('[data-italian');
 
+    regions.forEach(region => {
+      region.addEventListener('click', function(){
 
-    console.log(lastDayData);
+          let regionSelectedName = region.attributes[2].value;
+
+          let regionSelectedFilter = lastDayData.filter(el => el.denominazione_regione == regionSelectedName)[0];
+
+          let regionData = document.getElementById('region-data');
+
+          let regionNewPositives = sorted.reverse()
+                                  .filter(el => el.denominazione_regione == regionSelectedName)
+                                  .map(el => [el.data, el.nuovi_positivi]);
+          
+          let regionNewPositivesDate = regionNewPositives.map(el => el[0]).map(el => el.split("T")[0].split("-").reverse().join("/"));
+
+          let maxRegionNewPositives = Math.max(...regionNewPositives.map(el => el[1]));
+
+          let regionChartCanvas = document.getElementById('region-data-chart');
+          let regionChartParent = document.getElementById('region-data-chart-container');
+
+          // Destroy previous chart instance if it exists
+          if (regionChartCanvas) {
+            regionChartCanvas.remove(); // Remove the canvas element
+            let newCanvas = document.createElement('canvas');
+            newCanvas.id = 'region-data-chart';
+            regionChartParent.appendChild(newCanvas);
+          }
+          
+          
+          regionData.innerHTML = 
+            `
+            <h3>${region.attributes[1].value}</h3>
+            <p>Total dead: ${regionSelectedFilter.deceduti}</p>
+            <p>New infected: ${regionSelectedFilter.nuovi_positivi}</p>
+            <p>Total positives: ${regionSelectedFilter.totale_positivi}</p>
+            <p>Total healed: ${regionSelectedFilter.dimessi_guariti}</p>
+            
+            
+            `;
+
+          regions.forEach(r => r.classList.remove("region-active"));
+  
+          region.classList.add("region-active");
+        
+
+          new Chart(
+            document.getElementById('region-data-chart'),
+            {
+              type: 'line',
+              options: {
+                animation: false,
+                plugins: {
+                  legend: {
+                    display: false
+                  }
+                }
+              },
+              data: {
+                labels: [...regionNewPositivesDate],
+                datasets: [
+                  {
+                    label: ' ',
+                    data: [...regionNewPositives.map(el => el[1])],
+                    backgroundColor: [ 
+                        'rgb(6, 59, 80)'
+                      ],
+                  }
+                  
+                ]
+              }
+            }
+          );
+
+      })
+
+  })
+
+    
 })
